6539aae8a17c2aa905235fa1d831681f59ace913
TeodorescuIoan <72543777+TeodorescuIoan@users.noreply.github.com>
2021-02-27 13:32:56 +0200

    Edit1

--- src/Model/Record.java
+++ src/Model/Record.java
@@ -1,4 +1,48 @@
 package Model;
 
 public class Record {
+    private final int RecordID;
+    private final int Price;
+    private final String ALbumName;
+    private final int InStock;
+    private final RecordType TypeOfRecord;
+
+    public Record(int recordID, int price, String ALbumName, int inStock, RecordType typeOfRecord) {
+        RecordID = recordID;
+        Price = price;
+        this.ALbumName = ALbumName;
+        InStock = inStock;
+        TypeOfRecord = typeOfRecord;
+    }
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "RecordID=" + RecordID +
+                ", Price=" + Price +
+                ", ALbumName='" + ALbumName + '\'' +
+                ", InStock=" + InStock +
+                ", TypeOfRecord=" + TypeOfRecord +
+                '}';
+    }
+
+    public int getRecordID() {
+        return RecordID;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public String getALbumName() {
+        return ALbumName;
+    }
+
+    public int getInStock() {
+        return InStock;
+    }
+
+    public RecordType getTypeOfRecord() {
+        return TypeOfRecord;
+    }
 }
---
+++ src/Model/RecordType.java
@@ -0,0 +1,7 @@
+package Model;
+
+public enum RecordType {
+    CD
+    , VINYL
+    , TAPE
+}
