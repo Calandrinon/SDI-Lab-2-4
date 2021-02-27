1e41481dbad8289c835b1b8f8b3e1c9d30d8d22a
Calandrinon <georgesut@yahoo.com>
2021-02-27 14:45:24 +0200

    Add first version of the UI
    It might not work properly when the controller will be added
    TODO: Replace the 'add <entity>' comments with the actual add methods from the controllers

--- src/UI/UI.java
+++ src/UI/UI.java
@@ -1,5 +1,84 @@
 package UI;
 
-public class UI {
+import Controller.Controller;
 
+import java.util.Scanner;
+import Model.Record;
+import Model.RecordType;
+import Model.User;
+
+import static Model.RecordType.*;
+
+public class UI {
+    private Controller controller;
+    private boolean running;
+
+    public UI(Controller controller) {
+        this.controller = controller;
+        this.running = true;
+    }
+
+    public void displayMenu() {
+        System.out.println("1. Add record");
+        System.out.println("2. Add user");
+        System.out.println("0. Exit");
+    }
+
+    public void addRecord() {
+        Scanner input = new Scanner(System.in);
+        int recordId = input.nextInt();
+        String name = input.nextLine();
+        int price = input.nextInt();
+        String recordTypeAsString = input.nextLine().toLowerCase();
+        RecordType recordType;
+
+        switch (recordTypeAsString) {
+            case "cd":
+                recordType = CD;
+                break;
+            case "vinyl":
+                recordType = VINYL;
+                break;
+            case "tape":
+                recordType = TAPE;
+                break;
+            default:
+                recordType = CD;
+        }
+        Record record = new Record(recordId, price, name, 1, recordType);
+        //add record in repo through the RecordController
+    }
+
+    public void addUser() {
+        Scanner input = new Scanner(System.in);
+
+        int userId = input.nextInt();
+        String firstName = input.nextLine();
+        String lastName = input.nextLine();
+
+        User user = new User(userId, firstName, lastName, 0);
+        //add user in repo through the UserController
+    }
+
+    public void run() {
+        Scanner input = new Scanner(System.in);
+        int option = 0;
+
+        while (this.running) {
+            this.displayMenu();
+            System.out.print("Enter an option: ");
+            input.nextInt();
+
+            switch (option) {
+                case 1:
+                    addRecord();
+                    break;
+                case 2:
+                    addUser();
+                    break;
+                default:
+                    System.out.println("Enter an option between 1 and 2.");
+            }
+        }
+    }
 }
