2ee351b5a33d3d18faebe35b15918fb40f2b412e
Calandrinon <georgesut@yahoo.com>
2021-03-04 21:57:06 +0200

    Add the ClientUI/ClientController classes
    They are untested at this point, but since this is done on a branch separated from the master branch it should be fine

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,21 +1,30 @@
 package Main;
 
+import Main.Controller.ClientController;
 import Main.Controller.RecordController;
+import Main.Controller.TransactionController;
 import Main.Controller.UserController;
 import Main.Model.Record;
+import Main.Model.Transaction;
 import Main.Model.User;
 import Main.Repository.InMemoryRepository;
+import Main.UI.ClientUI;
 import Main.UI.UI;
 import Main.Validator.RecordValidator;
+import Main.Validator.TransactionValidator;
 import Main.Validator.UserValidator;
 
 public class Main {
     public static void main(String[] args) {
-
+        InMemoryRepository<Integer, Transaction> transactionRepository = new InMemoryRepository<Integer, Transaction>(new TransactionValidator());
+        InMemoryRepository<Integer, Record> clientRecordRepository = new InMemoryRepository<Integer, Record>(new RecordValidator());
         InMemoryRepository<Integer, Record> recordRepository = new InMemoryRepository<Integer, Record>(new RecordValidator());
         InMemoryRepository<Integer, User> userRepository = new InMemoryRepository<Integer, User>(new UserValidator());
+        TransactionController transactionController = new TransactionController(recordRepository, userRepository, transactionRepository);
+        ClientController clientController = new ClientController(clientRecordRepository, recordRepository);
         UserController userController = new UserController(userRepository);
         RecordController recordController = new RecordController(recordRepository);
+        ClientUI clientUserInterface = new ClientUI(clientController, recordController, transactionController, 1);
         UI userInterface = new UI(userController, recordController);
         userInterface.run();
     }
